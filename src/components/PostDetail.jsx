e548c8ebb1dbdf07c691a79979073fcfc8cb239d
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-08-30 23:03:39 +0400

    Add datetime to posts and comments

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -7,7 +7,7 @@ const PostDetail = (props) => {
       <Card className="my-3">
         <Card.Body>
           <Card.Title>{props.post.title}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">{props.post.author.username}</Card.Subtitle>
+          <Card.Subtitle className="mb-2 text-muted">{props.post.author.username} {props.post.created}</Card.Subtitle>
           <Card.Text>
             {props.post.content}
           </Card.Text>
@@ -16,7 +16,7 @@ const PostDetail = (props) => {
       <div>
         {props.comments.map(comm =>
           <div key={comm.id}>
-            <div>{comm.author.username}</div>
+            <div>{comm.author.username} {comm.created}</div>
             <div>{comm.content}</div>
           </div>
         )}
