e4e30578133cc2bfd564a8d38a2b88e2144864df
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-09-10 13:58:29 +0300

    Tweak navbar a bit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { AuthContext } from '../context';
 import { useFetching } from '../hooks/useFetching';
@@ -7,6 +7,8 @@ import AuthService from '../services/AuthService';
 
 const Header = () => {
   const { isAuth, setIsAuth } = useContext(AuthContext);
+  const username = localStorage.getItem('username');
+  const userid = localStorage.getItem('userid');
 
   const [logout, isLoading, error] = useFetching(async () => {
     await AuthService.logout();
@@ -24,20 +26,19 @@ const Header = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={NavLink} to="/" exact>Посты</Nav.Link>
-            {!isAuth
-              ? <Nav.Link as={NavLink} to="/login">Войти</Nav.Link>
-              : <React.Fragment>
-                <Nav.Link as={NavLink} to={`/users/${localStorage.getItem('userid')}`}>Личный кабинет</Nav.Link>
-                <Nav.Link onClick={logout}>Выйти</Nav.Link>
-              </React.Fragment>
-
-            }
           </Nav>
         </Navbar.Collapse>
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            {localStorage.getItem('username')}
-          </Navbar.Text>
+          <Nav>
+            {isAuth
+              ? <NavDropdown title={username} id="basic-nav-dropdown">
+                <NavDropdown.Item as={NavLink} to={`/users/${userid}`}>Личный кабинет</NavDropdown.Item>
+                <NavDropdown.Item onClick={logout}>Выйти</NavDropdown.Item>
+              </NavDropdown>
+              : <Nav.Link as={NavLink} to="/login">Войти</Nav.Link>
+            }
+          </Nav>
+
         </Navbar.Collapse>
       </Container>
     </Navbar>
