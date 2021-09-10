ac386d47855507e823cd9de8dce0230ea2fb7643
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-09-10 17:32:43 +0300

    Fix post detail link

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -11,7 +11,7 @@ const Post = (props) => {
         <Card.Text>
           {props.post.content}
         </Card.Text>
-        <Card.Link as={Link} to={`${props.post.id}`}>Перейти к посту</Card.Link>
+        <Card.Link as={Link} to={`/${props.post.id}`}>Перейти к посту</Card.Link>
       </Card.Body>
       <Card.Footer className="text-muted">Комментариев: {props.post.comment_count}</Card.Footer>
     </Card>
