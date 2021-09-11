1fee681c5852f515ad1c88a8ca3dfe8689994cd5
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-09-11 16:47:32 +0300

    Remove debug info

--- src/utils/time.js
+++ src/utils/time.js
@@ -7,9 +7,5 @@ export const timePassed = (time_created) => {
 }
 
 export const addHours = (time, hours = Date().getTimezoneOffset) => {
-    time = '11.09.2021 13:01'
-    console.log(time)
-    const date = moment.tz(time, "DD.MM.YYYY hh:mm")
-    console.log(date);
     return moment.tz(time, "DD.MM.YYYY hh:mm", Date().getTimezoneOffset).format('MM.DD.YYYY');
 }
