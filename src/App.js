3cd32968fed84dd0917f782f9e56b97b16438fbb
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-09-14 22:38:11 +0300

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/UI/Header"
 import Router from "./components/Router";
@@ -7,8 +6,7 @@ import LoaderError from "./components/UI/LoaderError";
 import { useAction } from "./hooks/useAction";
 
 function App() {
-  const user = useSelector(state => state);
-  const { logoutUser, refreshUser } = useAction();
+  const { refreshUser } = useAction();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
