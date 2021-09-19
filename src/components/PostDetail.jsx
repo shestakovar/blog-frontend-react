76688298ee870d4f247934570995b40c939a5cd1
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-09-19 19:38:03 +0300

    Make author of post clickable

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import TimeString from './UI/TimeString';
 
 const PostDetail = ({ post }) => {
@@ -10,7 +11,7 @@ const PostDetail = ({ post }) => {
     <Card className="my-3">
       <Card.Body>
         <Card.Title>{post.title}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{post.author} <TimeString string={post.created} /></Card.Subtitle>
+        <Card.Subtitle className="mb-2 text-muted"><Link to={`/users/${post.author_id}`} className="link-dark">{post.author}</Link> <TimeString string={post.created} /></Card.Subtitle>
         <Card.Text as="div">
           {post.content.split('\n').map((str, index) => <p key={index}>{str}</p>)}
         </Card.Text>
--- src/components/PostListItem.jsx
+++ src/components/PostListItem.jsx
@@ -11,7 +11,7 @@ const PostListItem = ({ post }) => {
     <Card className="my-3">
       <Card.Body>
         <Card.Title>{post.title}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{post.author} <TimeString string={post.created} /></Card.Subtitle>
+        <Card.Subtitle className="mb-2 text-muted"><Link to={`/users/${post.author_id}`} className="link-dark">{post.author}</Link> <TimeString string={post.created} /></Card.Subtitle>
         <Card.Text as="div" className="card_text">
           {post.content.split('\n').map((str, index) => <p key={index}>{str}</p>)}
         </Card.Text>
