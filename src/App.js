7d198cbc1e2bf471fe29fb28e6c8925a94f55a8e
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-09-21 15:53:55 +0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/UI/Header"
 import Router from "./components/Router";
+import Footer from "./components/UI/Footer";
 import LoaderError from "./components/UI/LoaderError";
 import { useAction } from "./hooks/useAction";
 import AuthService from "./services/AuthService";
 import { fillLocalStorage } from "./store/localStorage";
+import classes from './App.module.css';
 
 
 function App() {
@@ -35,8 +37,13 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Header />
-      <Router />
+      <div className={classes.container}>
+        <Header />
+        <div className={classes.main}>
+          <Router />
+        </div>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/components/UI/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+import { Telegram, Github, Instagram } from 'react-bootstrap-icons';
+
+const Footer = () => {
+  return (
+
+    <footer className="mt-5 text-center text-lg-start bg-light text-muted ">
+      <Container>
+        <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+
+          <div className="me-5 d-none d-lg-block">
+            <span>Свяжитесь со мной:</span>
+          </div>
+
+
+          <div>
+
+            <a href="https://t.me/shestakovar" className="me-4 text-reset">
+              <Telegram />
+            </a>
+            <a href="https://github.com/shestakovar" className="me-4 text-reset">
+              <Github />
+            </a>
+
+          </div>
+
+        </section>
+      </Container>
+    </footer>
+
+  )
+}
+
+export default Footer;
