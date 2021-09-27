c7fe0edf1dacca90ef28300544ec3118da5a6bf7
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-09-27 22:23:18 +0400

    Remove tz because moment do it by itself

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -5,11 +5,9 @@ moment.locale('ru');
 export const timePassed = (time_created: string | null) => {
     if (!time_created)
         return null;
-    const date = new Date();
-    const tz_offset = date.getTimezoneOffset().toString();
-    return moment.tz(time_created, "DD.MM.YYYY hh:mm", tz_offset).fromNow()
+    return moment.tz(time_created, "DD.MM.YYYY hh:mm", '').fromNow()
 }
 
-export const addHours = (time: string, hours = (new Date()).getTimezoneOffset.toString()) => {
-    return moment.tz(time, "DD.MM.YYYY hh:mm", hours).format('D MMMM YYYY');
+export const addHours = (time: string) => {
+    return moment.tz(time, "DD.MM.YYYY hh:mm", '').format('D MMMM YYYY');
 }
